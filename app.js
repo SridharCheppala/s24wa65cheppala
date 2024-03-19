6c2a55067dbd5d8ddb0eb3bda4c83291bda54013
SridharCheppala <sricheppala@gmail.com>
2024-03-19 18:41:12 -0500

    c1

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ var logger = require('morgan');
  
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var myDataRouter = require('./routes/mydata');
-//var computationRouter = require('./routes/computation');
+
  
- 
- 
+
 var app = express();
  
 // view engine setup
@@ -24,9 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
  
 app.use('/', indexRouter);
-app.use('/mydata', myDataRouter);
 app.use('/users', usersRouter);
-//app.use('/computation', computationRouter);
+
  
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
