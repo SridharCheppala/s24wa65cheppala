eb7b1350f57a8736cf6991da2e6e0475e4bbc21a
SridharCheppala <sricheppala@gmail.com>
2024-03-19 19:53:02 -0500

    1234

--- routes/computation.js
+++ routes/computation.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-let x= Math.random()*100;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    let x= Math.random() *100;
   let y = Math.cbrt(x);
   
   res.send(`cbrt of ${x} is ${y}`);
