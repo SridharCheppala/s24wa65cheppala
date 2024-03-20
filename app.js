b67b30bb4d55b34291c8cdef5efd1281bc0eb21f
SridharCheppala <sricheppala@gmail.com>
2024-03-20 11:50:22 -0500

    c

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
  
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var mydataRouter = require('./routes/myData');
+var mydataRouter = require('./routes/mydata');
 var computationRouter = require('./routes/computation');
 
 
